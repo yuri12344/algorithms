335a7e060aa3046cfbe984dc961bb9b8b26e2d60
Yuri Caetano <yuuri.caetano@gmail.com>
2025-07-20 22:11:48 -0300

    feat: finish hashtable and insert some tests

---
+++ hashtable/main.py
@@ -0,0 +1,123 @@
+# Aux link: https://aistudio.google.com/prompts/1ScsJ1OxsGaL78c__F0y4xpGs1GfvFes5
+import ipdb
+
+
+class HashTable:
+    RE_HASH_FACTOR = 0.7
+    RE_HASH_SIZE_MULTIPLY = 2
+
+    def __init__(self, size=10):
+        self.size = size
+        self.slots = [[] for _ in range(self.size)]
+        self.item_count = 0 
+
+    def hash(self, key: str):
+        total = sum(ord(char) for char in key)
+        return total % self.size
+
+    def _slot_available(self):
+        return sum(1 for n in self.slots if not n)
+    
+    def _add(self, _hash, item):
+        self.slots[_hash].append(item)
+        self.item_count += 1
+
+    def _update(self, _hash, key, item = None):
+        for index, stored_object in enumerate(self.slots[_hash]):
+            if key in stored_object:
+                if not item:
+                    self.slots[_hash].pop(index)
+                    self.item_count -= 1
+                else:
+                    self.slots[_hash][index] = item
+
+    def delete(self, key):
+        _hash = self.hash(key)
+        stored = self.search(key)
+        if stored:
+            self._update(_hash, key)
+
+    def store(self, item):
+        for key in item.keys():
+            # 1. Check if object exists
+            stored = self.search(key)
+            _hash = self.hash(key)
+
+            if stored:
+                self._update(_hash, key, item)
+            else:
+                self._add(_hash, item)
+
+        if self._should_re_hash():
+            self._re_hash()
+
+    def _re_hash(self):
+        self.size = self.size * self.RE_HASH_SIZE_MULTIPLY
+        self.item_count = 0 
+        old_slots = self.slots
+        self.slots = [[] for _ in range(self.size)]
+
+        for slot in old_slots:
+            for item in slot:
+                key = list(item.keys())[0]
+                _hash = self.hash(key)
+                self.slots[_hash].append(item)
+                self.item_count += 1
+
+    def search(self, key: str):
+        _hash = self.hash(key)
+        slot = self.slots[_hash]
+        for item in slot:
+            if key in item:
+                return item
+        return None
+
+    def _load_factor(self):
+        if self.item_count == 0:
+            return 0 
+        return self.item_count / self.size
+
+    def _should_re_hash(self):
+        factor = self._load_factor()
+        if factor > self.RE_HASH_FACTOR:
+            return True
+        else:
+            return False
+
+    def show(self):
+        for i, slot in enumerate(self.slots):
+            print(f"Slot {i}: {slot}")
+
+table = HashTable(5)
+person = {
+    "yuri caetano": "CPF: 221.509.123-09"
+}
+person2 = {
+    "yuari caetano": "CPF: 221.509.123-09"
+}
+table.store(person)
+table.store(person2)
+person = table.search('yuri caetano')
+table.show()
+print("=" * 10)
+
+
+update_yuri= {
+    "yuri caetano": "CPF: 222.222.222.-59"
+}
+table.store(update_yuri)
+table.store({'yuri caetano2': '119841981299'})  # collision
+table.show()
+
+
+print("\n" + "=" * 10)
+print("--- Testing DELETION ---")
+print("HashTable BEFORE deleting 'yuri caetano2':")
+table.show()
+
+# Call to your new function!
+table.delete('yuri caetano2')
+
+print("\nHashTable AFTER deleting 'yuri caetano2':")
+table.show()
+ipdb.set_trace()
